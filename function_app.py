3894c2d21b8dd7134b7aff08a7b6a3ca64618f24
left-katsu <satekats@fsi.co.jp>
2024-12-10 19:42:22 +0900

    コミット

--- function_app.py
+++ function_app.py
@@ -3,13 +3,11 @@ import logging
 
 app = func.FunctionApp()
 
-@app.blob_trigger(arg_name="myblob", path="mycontainer/ctn-kaitori",
-                               connection="Connection_String",
-                               source="EventGrid") 
-def BlobTriggerEventGrid(myblob: func.InputStream):
+@app.blob_trigger(arg_name="myblob", path="ctn-uriage",
+                               connection="snowpipeuriagecsvstorage_STORAGE") 
+def afunc_kaitori(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob"
                 f"Name: {myblob.name}"
                 f"Blob Size: {myblob.length} bytes")
-
     file = myblob.read()
     logging.info(f"File Content: {file}")
